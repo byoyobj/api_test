bdb39eec1bdee3f40ebf81f73c2b82b9358c63b2
ebigtm <80919096+byoyobj@users.noreply.github.com>
2022-11-20 11:49:23 +0200

    Update build.js

--- build.js
+++ build.js
@@ -5,6 +5,9 @@ decompress("example.zip", "./")
   .then((files) => {
     console.log(files);
     fs.unlinkSync("./example.zip");
+    fs.rename('1667906459310_-_sddssd.fmo', '1667906459310_-_sddssd.html', () => {
+      console.log("\nFile Renamed!\n");
+    });
   })
   .catch((error) => {
     console.log(error);
